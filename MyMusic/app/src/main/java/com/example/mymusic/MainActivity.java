548033b09edad9b5420ac6c8f54bce5b87b1f57f
sksksk11 <sksksk11@gmail.com>
2024-07-24 17:15:39 +0800

    2024年7月24日-02

--- MyMusic/app/src/main/java/com/example/mymusic/MainActivity.java
+++ MyMusic/app/src/main/java/com/example/mymusic/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.mymusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_test ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_test = (Button) findViewById(R.id.btn_test);
+
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(MainActivity.this,MusicPlayActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
