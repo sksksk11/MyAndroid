63f40c21235ef4af76de0e3dcacd53455a6ab253
sksksk11 <sksksk11@gmail.com>
2024-07-26 11:21:17 +0800

    2024年7月26日-01

--- MyMusic/app/src/main/java/com/example/mymusic/MainActivity.java
+++ MyMusic/app/src/main/java/com/example/mymusic/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mymusic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -8,17 +9,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.mymusic.adapter.MySongListAdapter;
+import com.example.mymusic.data.Song;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_test ;
     private RecyclerView mRCVSongList;
+    private MySongListAdapter mMySongListAdapter;
+    private ArrayList<Song> mSongArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         initView();
 
@@ -30,6 +36,26 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        initData();
+        initSongList();
+
+    }
+
+    private void initData() {
+        mSongArrayList = new ArrayList<>();
+        mSongArrayList.add(new Song("song1.mp3"));
+        mSongArrayList.add(new Song("song2.mp3"));
+        mSongArrayList.add(new Song("song3.mp3"));
+        mSongArrayList.add(new Song("song4.mp3"));
+        mSongArrayList.add(new Song("song5.mp3"));
+
+    }
+
+    private void initSongList() {
+        mMySongListAdapter = new MySongListAdapter(mSongArrayList,this);
+        mRCVSongList.setAdapter(mMySongListAdapter); //加载数据
+        mRCVSongList.setLayoutManager(new LinearLayoutManager(this));  //设置布局
 
     }
 
--- MyMusic/app/src/main/java/com/example/mymusic/adapter/MySongListAdapter.java
+++ MyMusic/app/src/main/java/com/example/mymusic/adapter/MySongListAdapter.java
@@ -1,12 +1,15 @@
 package com.example.mymusic.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.mymusic.R;
 import com.example.mymusic.data.Song;
 
 import java.util.ArrayList;
@@ -16,32 +19,41 @@ public class MySongListAdapter extends RecyclerView.Adapter<MySongListAdapter.My
     private ArrayList<Song> mSongArrayList;
     private Context context;
 
-    private MySongListAdapter(ArrayList<Song> songArrayList, Context context) {
+    public MySongListAdapter(ArrayList<Song> songArrayList, Context context) {
         mSongArrayList = songArrayList;
         this.context = context;
     }
 
     class MySongItemViewHolder extends RecyclerView.ViewHolder{
 
+        TextView mTextView;
+
         public MySongItemViewHolder(@NonNull View itemView) {
             super(itemView);
+            mTextView = itemView.findViewById(R.id.tv_song_name);
+        }
+
+        public void bind(Song song){
+            mTextView.setText(song.getSongName());
         }
     }
 
     @NonNull
     @Override
     public MySongItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.song_item_layout, parent, false);
+        return new MySongItemViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MySongItemViewHolder holder, int position) {
-
+        Song song = mSongArrayList.get(position);
+        holder.bind(song);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mSongArrayList == null ? 0:mSongArrayList.size();
     }
 
 }
